685450ded7c357e2c1c761fa683e0550cb4bf331
Ivan Gonzalez <ivanezeqgonzalez@github.com>
2019-03-27 16:19:53 -0300

    add npm Build to package.json & move files to src

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import TodoController from '../controllers/todo.controller';
 
 const router = express.Router();
-
 router.get('/api/v1/todos', TodoController.getAllTodos);
 router.get('/api/v1/todos/:id', TodoController.getTodo);
 router.post('/api/v1/todos', TodoController.createTodo);
